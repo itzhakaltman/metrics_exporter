af782a45dbf3be175c5ec5be27ba0bef8c464505
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2021-12-18 23:02:12 +0200

    1.1.0

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,11 @@
-# first stage
 FROM python:3.8 AS builder
 COPY requirements.txt .
-
-# install dependencies to the local user directory (eg. /root/.local)
 RUN pip install --user -r requirements.txt
-
-# second unnamed stage
 FROM python:3.8-slim
 WORKDIR /code
-
-# copy only the dependencies that are needed for our application and the source files
 COPY --from=builder /root/.local /root/.local
 COPY ./src .
-
-EXPOSE 8000
-EXPOSE 8001
-
-# update PATH
+EXPOSE 8080
+EXPOSE 8090
 ENV PATH=/root/.local:$PATH
-
-# make sure you include the -u flag to have our stdout logged
 CMD ["python", "-u", "./main.py"]
--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ from prometheus_client import start_http_server, Counter
 REQUEST_COUNT = Counter('app_requests_count', 'total app http request count', ['app_name', 'endpoint'])
 RANDOM_COUNT = Counter('app_random_count', 'increment counter by random value')
 
-APP_PORT = 8000
-METRICS_PORT = 8001
-
+APP_PORT = 8080
+METRICS_PORT = 8090
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
 
@@ -19,13 +18,10 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes(
-            "<!DOCTYPE html><html><head><title>metrics_generator</title><style>body{width:30em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-serif;}</style></head><body><h1>Welcome to the metrics generator!</h1><p><em>Phantasie ist wichtiger als Wissen. Wissen ist begrenzt, Phantasie aber umfa&#223t die ganze Welt.</em></p><p><em>Albert Einstein.</em></p></body></html>",
-            "utf-8"))
-        self.wfile.close()
-
+        self.wfile.write(bytes("<!DOCTYPE html><html><head><title>metrics_generator</title><style>body{width:30em;margin: 0 auto;font-family: Tahoma, Verdana, Arial, sans-serif;}</style></head><body><h1>Welcome to the metrics generator!</h1><p><em>Phantasie ist wichtiger als Wissen. Wissen ist begrenzt, Phantasie aber umfa&#223t die ganze Welt.</em></p><p><em>Albert Einstein.</em></p></body></html>","utf-8"))
+#        self.wfile.close()
 
 if __name__ == "__main__":
     start_http_server(METRICS_PORT)
-    server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    server = http.server.HTTPServer(('0.0.0.0', APP_PORT), HandleRequests)
     server.serve_forever()
