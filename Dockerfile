51f8917900ef5e732f83d25d9bce617eaa82bf8d
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2021-12-29 07:07:30 +0200

    1.1.48

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 # docker buildx build -t metrics_exporter . --platform linux/amd64
+# docker tag metrics_exporter zukunft/metrics_exporter:1.1.2
 
 FROM python:3.8 AS builder
 COPY requirements.txt .
@@ -7,6 +8,7 @@ FROM python:3.8-slim
 WORKDIR /code
 COPY --from=builder /root/.local /root/.local
 COPY ./src .
+COPY ./config .
 EXPOSE 8080
 EXPOSE 8090
 ENV PATH=/root/.local:$PATH
--- src/main.py
+++ src/main.py
@@ -3,25 +3,41 @@
 # https://towardsdatascience.com/from-novice-to-expert-how-to-write-a-configuration-file-in-python-273e171a8eb3
 
 import yaml
-import os
 
-#config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
+# import os
 
-dir_path = os.path.join(os.path.dirname( __file__ ), '..' )
-config_path = os.path.join(dir_path, "config", "config.yaml")
-print(config_path)
+config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
 
-#def read_yaml():
+
+# dir_path = os.path.join(os.path.dirname(__file__), '..')
+# config_path = os.path.join(dir_path, "config", "config.yaml")
+# print(config_path)
+
+def read_yaml(config_file):
 #    dir_path = os.path.dirname(os.path.realpath(__file__))
 #    companies_path = os.path.join(dir_path, "config", "config.yaml")
-#    with open(config_file, "r") as f:
-#        return yaml.safe_load(f)
+    with open(config_file, "r") as f:
+        return yaml.safe_load(f)
+
+
+print(read_yaml(config_file))
+
+#ENVIRONMENT = : dev
+#  DEBUG: True #False
+
+#PUSHGATEWAY:
+#  HOST: 192.168.192.2
+#  PORT: 8080
+#
+#DATABASE:
+#  USERNAME: user
+#  PASSWORD: password
+#  HOST: 192.168.192.3
+#  PORT: 5432
+#  DB: metrics_exporter
 
 
 
 
-
-#print(read_yaml(config_file))
-#if __name__ == "__main__":
+# if __name__ == "__main__":
 #    read_yaml(config_file)
-
