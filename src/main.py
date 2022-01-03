83fc98273f876c8d0e6764861a714f296f5b4e1e
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-03 07:04:30 +0200

    1.1.52

--- src/main.py
+++ src/main.py
@@ -5,20 +5,35 @@ import subprocess
 
 config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
 
-
 def read_yaml(config_file):
     with open(config_file, "r") as f:
         return yaml.safe_load(f)
-
 
 config = read_yaml(config_file)
 print(config)
 # print(config["HOST"])
 # print(config["PORT"])
 
+pgw_host = config["PGW HOST"]
+pgw_port = config["PGW PORT"]
+job_name = config["JOB NAME"]
+label_name = config["LABEL NAME"]
+label_value = config["LABEL VALUE"]
+kafka = config["KAFKA"]
+topic_port = config["TOPIC PORT"]
+topic = config["TOPIC"]
+key = config["KEY"]
+metric_name = config["METRIC NAME"]
+metric_value = config["METRIC VALUE"]
 
-host = config["HOST"]
+# BODY - echo “demo_metric 123”
 
-curl = subprocess.run(['curl', 'http://{}/'.format(host)])
+#curl = subprocess.run(['curl',
+#                       'http://{}:{}/metrics/job/{}/{}/{}/event/add'.format(pgw_host, pgw_port, job_name, label_name,
+#                                                                            label_value)])
+# curl -X POST --header "Content-Type: application/json" --data '{"key1":"value1", "key2":"value2"}' http://8.8.8.8//
+
+curl = ['curl', 'http://{}:{}/metrics/job/{}/{}/{}/event/add'.format(pgw_host, pgw_port, job_name, label_name, label_value)]
+
 
 print(curl)
