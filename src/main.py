045e447f26cd7291f349f4df64bc08a203518a6e
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-04 06:59:24 +0200

    1.1.53

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import yaml
-import subprocess
+import requests
 
 config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
 
@@ -11,29 +11,40 @@ def read_yaml(config_file):
 
 config = read_yaml(config_file)
 print(config)
-# print(config["HOST"])
-# print(config["PORT"])
 
-pgw_host = config["PGW HOST"]
-pgw_port = config["PGW PORT"]
-job_name = config["JOB NAME"]
-label_name = config["LABEL NAME"]
-label_value = config["LABEL VALUE"]
-kafka = config["KAFKA"]
-topic_port = config["TOPIC PORT"]
-topic = config["TOPIC"]
-key = config["KEY"]
-metric_name = config["METRIC NAME"]
-metric_value = config["METRIC VALUE"]
+PGW_HOST = config["PGW HOST"]
+PGW_PORT = config["PGW PORT"]
+JOB_NAME = config["JOB NAME"]
+LABEL_NAME = config["LABEL NAME"]
+LABEL_VALUE = config["LABEL VALUE"]
+KAFKA = config["KAFKA"]
+TOPIC_PORT = config["TOPIC PORT"]
+TOPIC = config["TOPIC"]
+KEY = config["KEY"]
+METRIC_NAME = config["METRIC NAME"]
+METRIC_VALUE = config["METRIC VALUE"]
+
+API_ENDPOINT = 'http://{}:{}/metrics/job/{}/{}/{}'.format(PGW_HOST, PGW_PORT, JOB_NAME, LABEL_NAME,
+                                                                    LABEL_VALUE)
+#data = '{} {}'.format(METRIC_NAME, METRIC_VALUE)
+data = 'demo 321'
+request = requests.post(url=API_ENDPOINT, data=data)
+
+print(API_ENDPOINT)
+print(data)
+print(request.text)
+
+# echo "demo_metric 123" | curl --data-binary @- http://localhost:9091/metrics/job/demo_pg_job/instance/demo_instance/event/add
+
 
 # BODY - echo “demo_metric 123”
 
-#curl = subprocess.run(['curl',
+# curl = subprocess.run(['curl',
 #                       'http://{}:{}/metrics/job/{}/{}/{}/event/add'.format(pgw_host, pgw_port, job_name, label_name,
 #                                                                            label_value)])
 # curl -X POST --header "Content-Type: application/json" --data '{"key1":"value1", "key2":"value2"}' http://8.8.8.8//
 
-curl = ['curl', 'http://{}:{}/metrics/job/{}/{}/{}/event/add'.format(pgw_host, pgw_port, job_name, label_name, label_value)]
+# curl = ['curl', 'http://{}:{}/metrics/job/{}/{}/{}/event/add'.format(pgw_host, pgw_port, job_name, label_name, label_value)]
 
 
-print(curl)
+# print(curl)
