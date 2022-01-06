a552daa2cf1f9bad24daea27802d55f89626c051
itzhakaltman <96288626+itzhakaltman@users.noreply.github.com>
2022-01-06 07:02:56 +0200

    1.1.55

---
+++ src/config.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import yaml
+
+config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
+
+
+def read_yaml(config_file):
+    with open(config_file, "r") as f:
+        return yaml.safe_load(f)
+
+
+config = read_yaml(config_file)
+print(config)
+
+PGW_HOST = config["PGW HOST"]
+PGW_PORT = config["PGW PORT"]
+JOB_NAME = config["JOB NAME"]
+LABEL_NAME = config["LABEL NAME"]
+LABEL_VALUE = config["LABEL VALUE"]
+KAFKA = config["KAFKA"]
+TOPIC_PORT = config["TOPIC PORT"]
+TOPIC = config["TOPIC"]
+KEY = config["KEY"]
+METRIC_NAME = config["METRIC NAME"]
+METRIC_VALUE = config["METRIC VALUE"]
+
+API_ENDPOINT = 'http://{}:{}/metrics/job/{}/{}/{}'.format(PGW_HOST, PGW_PORT, JOB_NAME, LABEL_NAME, LABEL_VALUE)
+# print(API_ENDPOINT)
+# print(request.text)
--- src/main.py
+++ src/main.py
@@ -3,29 +3,15 @@
 import yaml
 import requests
 
-config_file = '/Users/itzhak/Documents/GitHub/metrics_exporter/config/config.yaml'
 
 def read_yaml(config_file):
     with open(config_file, "r") as f:
         return yaml.safe_load(f)
 
+
 config = read_yaml(config_file)
 print(config)
 
-PGW_HOST = config["PGW HOST"]
-PGW_PORT = config["PGW PORT"]
-JOB_NAME = config["JOB NAME"]
-LABEL_NAME = config["LABEL NAME"]
-LABEL_VALUE = config["LABEL VALUE"]
-KAFKA = config["KAFKA"]
-TOPIC_PORT = config["TOPIC PORT"]
-TOPIC = config["TOPIC"]
-KEY = config["KEY"]
-METRIC_NAME = config["METRIC NAME"]
-METRIC_VALUE = config["METRIC VALUE"]
-
-API_ENDPOINT = 'http://{}:{}/metrics/job/{}/{}/{}/new_label/new_value'.format(PGW_HOST, PGW_PORT, JOB_NAME, LABEL_NAME,
-                                                                              LABEL_VALUE)
 data = 'demo 321\n'
 
 request = requests.post(url=API_ENDPOINT, data=data)
